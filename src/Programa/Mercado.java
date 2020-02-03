ef6cbc472fda2fdc06dee4682f080bd9562a24b6
Flavio Santana <flavio_3310@hotmail.com>
2020-02-03 16:48:00 -0300

    Projeto1

---
+++ src/Programa/Mercado.java
@@ -0,0 +1,61 @@
+package Programa;
+
+public class Mercado {
+
+	
+		public static void main(String[] args) {
+			   
+		    String Ma= " Macarrão ";
+		    String Fe = " Feijão ";
+		    String Ar = " Arroz ";
+		    String Ol = " Óleo ";
+		    String Des = " Desifetante ";
+		    String Dt = " Detergente ";
+		    String Sa = " Sabonete ";
+		    String Sb = " Sabão em pó ";
+		    String Pas = " Pasta de dente ";
+		    String Fra = " Frango ";
+		    
+		    double preço1 = 3.25;
+		    double preço2 = 4.00;
+		    double preço3 = 3.00;
+		    double preço4 = 2.56;
+		    double preço5 = 1.99;
+		    double preço6 = 1.59;
+		    double preço7 = 0.99;
+		    double preço8 = 4.89;
+		    double preço9 = 2.99;
+		    double preço10 = 9.99;
+		     System.out.println(" Primeiro item" + Ma);
+		     System.out.printf (" Valor: %.2f%n ", + preço1);
+		     System.out.println(" Segundo item " + Fe);
+		     System.out.printf (" Valor: %.2f%n ", + preço2);
+		     System.out.println(" Terceiro item " + Ar);
+		     System.out.printf (" Valor: %.2f%n ", + preço3);
+		     System.out.println(" Quarto item " + Ol);
+		     System.out.printf (" Valor: %.2f%n ", + preço4);
+		     System.out.println(" Quinto item " + Des);
+		     System.out.printf (" Valor: %.2f%n ", + preço5);
+		     System.out.println(" Sexto item " + Dt);
+		     System.out.printf (" Valor: %.2f%n ", + preço6);
+		     System.out.println(" Setimo item " + Sa);
+		     System.out.printf (" Valor: %.2f%n ", + preço7);
+		     System.out.println(" Oitavo item " + Sb);
+		     System.out.printf (" Valor: %.2f%n ", + preço8);
+		     System.out.println(" Nono item " + Pas);
+		     System.out.printf (" Valor: %.2f%n ", + preço9);
+		     System.out.println(" Decimo item " + Fra);
+		     System.out.printf (" Valor: %.2f%n ",  + preço10);
+		    
+		    double total = 35.00;
+		     
+		      if (total <40) {
+		     System.out.printf(" Levara brindes: %.2f%n",  + total);
+		      }
+		      else{
+		          System.out.printf(" Não levara brindes: %.2f%n", + total);
+		      }
+		      
+		   }
+
+		}
